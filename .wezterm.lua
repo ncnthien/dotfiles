ec2634b3f48ac76b4e08cd1374311dafbb74f5da
ncnthien <nhatthien185@gmail.com>
2023-12-22 17:51:21 +0700

    main: update tab theme for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,6 +33,18 @@ config.colors = {
 	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
 	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
 	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+	tab_bar = {
+		background = "#282a36",
+		active_tab = {
+			bg_color = "#44475A",
+			fg_color = "#ddd7ba",
+			intensity = "Bold",
+		},
+		inactive_tab = {
+			bg_color = "#282a36",
+			fg_color = "#ddd7ba",
+		},
+	},
 }
 
 return config
