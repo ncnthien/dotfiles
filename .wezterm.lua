c6613972c4656019ca04d2f92e0f3341fc92d5af
ncnthien <nhatthien185@gmail.com>
2023-12-22 14:49:54 +0700

    main: update theme for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,27 +10,29 @@ end
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
 config.font_size = 14
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.force_reverse_video_cursor = true
 
 -- Config tab_bar
 config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.colors = {
-	background = "#1e1f28",
-	tab_bar = {
-		background = "#282a36",
-		active_tab = {
-			bg_color = "#44475A",
-			fg_color = "#ddd7ba",
-			intensity = "Bold",
-		},
-		inactive_tab = {
-			bg_color = "#282a36",
-			fg_color = "#ddd7ba",
-		},
-	},
-	cursor_bg = "#ddd7ba",
-	cursor_border = "#ddd7ba",
+	foreground = "#dcd7ba",
+	background = "#1f1f28",
+
+	cursor_bg = "#c8c093",
+	cursor_fg = "#c8c093",
+	cursor_border = "#c8c093",
+
+	selection_fg = "#c8c093",
+	selection_bg = "#2d4f67",
+
+	scrollbar_thumb = "#16161d",
+	split = "#16161d",
+
+	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
 }
 
 return config
