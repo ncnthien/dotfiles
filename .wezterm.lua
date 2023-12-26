16e9764e661f12f168681016f3a65b8c6e5d19c9
ncnthien <nhatthien185@gmail.com>
2023-12-26 11:24:36 +0700

    main: update fontsize for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ end
 
 -- Settings
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
-config.font_size = 14
+config.font_size = 20
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.force_reverse_video_cursor = true
 
