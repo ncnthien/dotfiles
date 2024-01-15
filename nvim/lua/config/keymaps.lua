53506709eb0b52d8a22952ab982dfb6e54fc5daf
ncnthien <nhatthien185@gmail.com>
2024-01-15 23:08:19 +0700

    main: update keymaps

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+local map = vim.keymap.set
+
+-- Select all
+map("n", "<C-a>", "gg<S-v>G")
+
+-- Escape to normal mode
+map("i", "jk", "<Esc>")
+map("v", "jk", "<Esc>")
+
+-- Disable arrow keys
+map("n", "<Up>", "<Nop>")
+map("n", "<Down>", "<Nop>")
+map("n", "<Left>", "<Nop>")
+map("n", "<Right>", "<Nop>")
+map("i", "<Up>", "<Nop>")
+map("i", "<Down>", "<Nop>")
+map("i", "<Left>", "<Nop>")
+map("i", "<Right>", "<Nop>")
