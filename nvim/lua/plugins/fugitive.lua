2074a495f8b204d674ad2a755385ec770327fbd5
ncnthien <nhatthien185@gmail.com>
2024-01-19 19:41:51 +1100

    ./nvim/lua/plugins/fugitive.luas

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,19 @@
 return {
-  "tpope/vim-fugitive"
+  "tpope/vim-fugitive",
+  name = "fugitive",
+  keys = {
+    { "<leader>gg", "<cmd>Git<cr>", desc = "Git status" },
+    { "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add all" },
+    { "<leader>gl", "<cmd>Git log<cr>", desc = "Git log" },
+    { "<leader>gd", "<cmd>Gvdiff<cr>", desc = "Git vertical diff" },
+    { 
+      "<leader>gcm", 
+      function()
+        vim.ui.input({ prompt = "Commit message: " }, function(input)
+          vim.cmd([[ Git commit -m %s ]], input)
+        end)
+      end, 
+      desc = "Git commit message" 
+    }
+  }
 }
