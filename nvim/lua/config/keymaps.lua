9b5ad64ff3987ffe9e7472375465ecfbaf5294c6
ncnthien <nhatthien185@gmail.com>
2024-01-19 21:38:44 +1100

    main: add key map for manipulating windows, save file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,11 +1,11 @@
 local map = vim.keymap.set
 
 -- Select all
-map("n", "<C-a>", "gg<S-v>G")
+map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
 -- Escape to normal mode
-map("i", "jk", "<Esc>")
-map("v", "jk", "<Esc>")
+map("i", "jk", "<Esc>", { desc = "Escape to normal mode" })
+map("v", "jk", "<Esc>", { desc = "Escape to normal mode" })
 
 -- Disable arrow keys
 map("n", "<Up>", "<Nop>")
@@ -17,4 +17,20 @@ map("i", "<Down>", "<Nop>")
 map("i", "<Left>", "<Nop>")
 map("i", "<Right>", "<Nop>")
 
+-- Quit all
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- Move to window
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+map("n", "<leader>wj", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>wl", "<C-W>v", { desc = "Split window right", remap = true })
+
+-- Save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
