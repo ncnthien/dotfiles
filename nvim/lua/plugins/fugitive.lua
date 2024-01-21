3a5081ade09d3f561e5dd82b1c9b5cb149e2b94e
ncnthien <nhatthien185@gmail.com>
2024-01-21 21:30:36 +1100

    main: add git push --force for fugitive keymap

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -22,6 +22,19 @@ return {
     },
     { "<leader>gca", "<cmd>Git commit --amend<cr>", desc = "Git commit amend" },
     { "<leader>gps", "<cmd>Git push<cr>", desc = "Git push" },
+    { 
+      "<leader>gpsf", 
+			function()
+				vim.ui.input({ prompt = "Do you really want to force push? (y/n): " }, function(input)
+					if input == 'y' then
+						vim.cmd('Git push -f')
+          else
+            print('Force push cancelled.')
+					end
+				end)
+			end,
+      desc = "Git push force" 
+    },
     { "<leader>gpl", "<cmd>Git pull<cr>", desc = "Git pull" },
   }
 }
