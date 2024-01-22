314b5ff59d789a59056d9a928136e86ac6b5f438
ncnthien <nhatthien185@gmail.com>
2024-01-22 21:37:47 +0700

    main: update autocmd for lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,7 +8,23 @@ return {
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
+        "lua_ls",
         "tsserver"
+      },
+      handlers = {
+        function(server_name)
+          local lspconfig = require("lspconfig")
+          lspconfig[server_name].setup({})
+        end
+      }
+    })
+    require('lspconfig').lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = {'vim'}
+          }
+        }
       }
     })
   end
