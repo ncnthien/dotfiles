bd88eb5784ac98e3fbbf5bdf9d8f65ed6fad56e8
ncnthien <nhatthien185@gmail.com>
2024-01-22 13:49:30 +0700

    main: update keymaps for fugitive

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -4,14 +4,14 @@ return {
   keys = {
     { "<leader>gg", "<cmd>Git<cr>", desc = "Git status" },
     { "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add all" },
-    { "<leader>gl", "<cmd>Git log<cr>", desc = "Git log" },
+    { "<leader>glg", "<cmd>Git log<cr>", desc = "Git log" },
     { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git vertical diff" },
     { "<leader>gb", "<cmd>Git branch<cr>", desc = "Git branch" },
     { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
     { "<leader>gca", "<cmd>Git commit --amend<cr>", desc = "Git commit amend" },
-    { "<leader>gps", "<cmd>Git push<cr>", desc = "Git push" },
+    { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
     { 
-      "<leader>gpsf", 
+      "<leader>gpf", 
 			function()
 				vim.ui.input({ prompt = "Do you really want to force push? (y/n): " }, function(input)
 					if input == 'y' then
@@ -23,6 +23,6 @@ return {
 			end,
       desc = "Git push force" 
     },
-    { "<leader>gpl", "<cmd>Git pull<cr>", desc = "Git pull" },
+    { "<leader>gl", "<cmd>Git pull<cr>", desc = "Git pull" },
   }
 }
