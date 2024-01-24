8448ebe5380501e257d34046fefc6e488c4b928a
ncnthien <nhatthien185@gmail.com>
2024-01-24 20:40:26 +0700

    main: tree-sitter config for lazy load

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = false,
   build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   config = function()
     require("nvim-treesitter.configs").setup({
       highlight = { enable = true },
