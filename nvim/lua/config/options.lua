619c4b2068896a2e683b1fc807caeeef491a0630
ncnthien <nhatthien185@gmail.com>
2024-01-26 15:09:27 +0700

    main: add transparent style

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,5 @@
 local opt = vim.opt
 
-opt.cursorline = true
 opt.expandtab = true
 opt.number = true
 opt.relativenumber = true
--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -4,6 +4,11 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+    require('rose-pine').setup({
+      styles = {
+        transparency = true,
+      }
+    })
 		vim.cmd([[colorscheme rose-pine]])
 	end
 }
