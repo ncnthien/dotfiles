1a61fcdf40a0a6308f0ed53c5fd9e5acec834b40
ncnthien <nhatthien185@gmail.com>
2024-03-07 10:47:40 +0700

    main: update keymap for oil, and S-CR for alacritty

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,21 +8,22 @@ return {
     delete_to_trash = true,
     use_default_keymaps = false,
     view_options = {
+      show_hidden = true,
       is_hidden_file = function(name)
         return vim.startswith(name, '.') or vim.endswith(name, 'node_modules')
       end,
     },
     keymaps = {
       ["g?"] = "actions.show_help",
-      ["-"] = "actions.select",
-      ["<cr>"] = {
+      ["<CR>"] = "actions.select",
+      ["-"] = {
         callback = function()
           require('oil').select({ vertical = true, split = "botright" })
         end,
         desc = "Select split vertical right",
         mode = "n"
       },
-      ["_"] = "actions.parent",
+      ["<S-CR>"] = "actions.parent",
       ["qq"] = "actions.close",
       ["<C-p>"] = "actions.preview",
       ["<C-r>"] = "actions.refresh",
