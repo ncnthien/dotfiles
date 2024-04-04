f20f5c47f1ee2df5f8094d7423f5ca2b9e58474e
ncnthien <nhatthien185@gmail.com>
2024-04-04 11:07:32 +0700

    main: map previous buffer to <leader>9

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -18,6 +18,6 @@ return {
     { "<leader>6", function() require("harpoon"):list():select(6) end, desc = "Harpoon select 6" },
     { "<leader>7", function() require("harpoon"):list():select(7) end, desc = "Harpoon select 7" },
     { "<leader>8", function() require("harpoon"):list():select(8) end, desc = "Harpoon select 8" },
-    { "<leader>9", function() require("harpoon"):list():select(9) end, desc = "Harpoon select 9" },
+    { "<leader>9", function() vim.cmd([[ b# ]]) end, desc = "Select previous buffer" },
   },
 }
