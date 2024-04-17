555e395d78983f7ebf821959a95d440720d947a4
ncnthien <nhatthien185@gmail.com>
2024-04-17 11:57:01 +0700

    main: add key for gitsigns hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,6 +4,8 @@ return {
   keys = {
     { "<leader>gsp", "<cmd>lua require('gitsigns').preview_hunk()<cr>", desc = "Preview Git hunk" },
     { "<leader>gsr", "<cmd>lua require('gitsigns').reset_hunk()<cr>", desc = "Reset Git hunk" },
+    { "]]", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk" },
+    { "[[", "<cmd>Gitsigns prev_hunk<cr>", desc = "Previous hunk" },
   },
   config = function ()
     require("gitsigns").setup()
