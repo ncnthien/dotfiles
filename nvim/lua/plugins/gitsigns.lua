021e2257fb3b1a38a44fa12483b36c9463dcaced
ncnthien <nhatthien185@gmail.com>
2024-05-29 23:29:32 +0700

    main: update keymap for reset hunk and preview hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,8 +2,8 @@ return {
   "lewis6991/gitsigns.nvim",
   event = "BufRead",
   keys = {
-    { "<leader>gsp", "<cmd>lua require('gitsigns').preview_hunk()<cr>", desc = "Preview Git hunk" },
-    { "<leader>gsr", "<cmd>lua require('gitsigns').reset_hunk()<cr>", desc = "Reset Git hunk" },
+    { "tp", "<cmd>lua require('gitsigns').preview_hunk()<cr>", desc = "Preview Git hunk" },
+    { "td", "<cmd>lua require('gitsigns').reset_hunk()<cr>", desc = "Reset Git hunk" },
     { "tj", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk" },
     { "tk", "<cmd>Gitsigns prev_hunk<cr>", desc = "Previous hunk" },
   },
