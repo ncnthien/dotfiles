e0dfe1a210ab006e855264663b55ef7279daa2ff
ncnthien <nhatthien185@gmail.com>
2024-06-27 19:55:46 +1000

    main: does not allow uppercase label

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,10 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   otps = {
-    labels = "asdfghjklwertyuioxcvbn"
+    labels = "asdfghjklwertyuioxcvbn",
+    label = {
+      uppercase = false,
+    }
   },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
