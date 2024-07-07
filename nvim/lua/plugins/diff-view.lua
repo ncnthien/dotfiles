c88d4ece8cd1503f045ce2b4e3cc6947c3c16e45
ncnthien <nhatthien185@gmail.com>
2024-07-07 18:50:57 +0700

    main: keymaps for diffview

--- nvim/lua/plugins/diff-view.lua
+++ nvim/lua/plugins/diff-view.lua
@@ -1,3 +1,9 @@
 return {
   "sindrets/diffview.nvim",
+  name = "diffview",
+  keys = {
+    { "<leader>df", ":DiffviewFileHistory %", desc = "Diffview current file history" },
+    { "<leader>dd", "<cmd>DiffviewClose<cr>", desc = "Diffview close" }
+  },
+  opts = { use_icons = false }
 }
