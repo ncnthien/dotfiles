1d6e409766d1dae4e31a462c35a1207fa485e51a
ncnthien <nhatthien185@gmail.com>
2024-07-09 22:20:39 +0700

    main: add transparent for alacritty and nightfox

--- nvim/lua/plugins/nightfox.lua
+++ nvim/lua/plugins/nightfox.lua
@@ -6,8 +6,10 @@ return {
   config = function()
     require("nightfox").setup({
       options = {
+        transparent = true,
       }
     })
     vim.cmd([[colorscheme nordfox]])
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
   end
 }
