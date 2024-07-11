31f6f0ecb3de947ecf47c98865d8bb41ac5d58ae
ncnthien <nhatthien185@gmail.com>
2024-07-11 19:22:30 +1000

    main: remove neoclip

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,9 +8,5 @@ return {
     { "<leader><space>", "<cmd>Telescope find_files<cr>",  desc = "Find files" },
     { "<leader>/",       "<cmd>Telescope live_grep<cr>",   desc = "Live grep",   mode = "n", },
     { "<leader>/",       "<cmd>Telescope grep_string<cr>", desc = "String grep", mode = "v", },
-    { "<leader>y",       "<cmd>Telescope neoclip<cr>",     desc = "Clipboard" },
-  },
-  config = function()
-    require("telescope").load_extension("neoclip")
-  end,
+  }
 }
