6b33f4fdc86f42b9a5de9ec037fe994f1ed27c07
ncnthien <nhatthien185@gmail.com>
2024-07-15 02:12:55 +1000

    main: update fzf config

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -2,10 +2,10 @@ return {
   "ibhagwan/fzf-lua",
   name = "fzf",
   keys = {
-    { "<leader>ff", ":FzfLua files<CR>", { noremap = true, silent = true, description = "files" } },
-    { "<leader>fl", ":FzfLua live_grep<CR>", { noremap = true, silent = true, description = "live_grep" } },
-    { "<leader>fw", ":FzfLua grep_cword<CR>", { noremap = true, silent = true, description = "grep_cword" } },
-    { "<leader>ca", ":FzfLua lsp_code_actions previewer=false<CR>", { noremap = true, silent = true, description = "code action" } }
+    { "<leader>ff", ":FzfLua files<cr>", { noremap = true, silent = true, description = "files" } },
+    { "<leader>fl", ":FzfLua live_grep<cr>", { noremap = true, silent = true, description = "live_grep" } },
+    { "<leader>fw", ":FzfLua grep_cword<cr>", { noremap = true, silent = true, description = "grep_cword" } },
+    { "<leader>ca", ":FzfLua lsp_code_actions previewer=false<cr>", { noremap = true, silent = true, description = "code action" } }
   },
   config = function()
     require("fzf-lua").setup({
@@ -15,7 +15,24 @@ return {
           ["<C-d>"] = "preview-page-down",
         }
       },
-      fzf_colors = true
+      fzf_colors = true,
+      winopts = {
+        border = "single",
+        preview = {
+          title = false,
+          scrollbar = false,
+          winopts = {
+            number = false
+          }
+        }
+      },
+      files = {
+        cwd_prompt = false,
+        formatter = "path.filename_first",
+      },
+      grep = {
+        formatter = "path.filename_first",
+      }
     })
   end
 }
