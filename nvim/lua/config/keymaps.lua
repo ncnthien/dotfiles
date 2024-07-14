14d443305133bc76394dbab6d4f570c62c3b3a5b
ncnthien <nhatthien185@gmail.com>
2024-07-15 01:16:36 +1000

    main: add keymap for better search in command

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -48,6 +48,16 @@ map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent =
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+-- Map space to .* in search command mode
+map("c", "<space>", function ()
+  local mode = vim.fn.getcmdtype()
+  if mode == "?" or mode == "/" then
+    return ".*"
+  else
+    return " "
+  end
+end, { expr = true })
+
 -- console.log current word (JS only)
 map(
   "n",
