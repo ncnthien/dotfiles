7a8045f182261fc907146ba02065b70e7b4f7251
ncnthien <nhatthien185@gmail.com>
2024-07-25 22:56:09 +0700

    main: update convention for using config in fzflua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -15,34 +15,32 @@ return {
     { "<leader>fe", "<cmd>FzfLua lsp_references<cr>", { noremap = true, silent = true, description = "references" } },
     { "<leader>fD", "<cmd>FzfLua lsp_declarations jump_to_single_result=true<cr>", { noremap = true, silent = true, description = "declarations" } },
   },
-  config = function()
-    require("fzf-lua").setup({
-      file_ignore_patterns = { ".git/", "node_modules/", "e2e/" },
-      keymap = {
-        builtin = {
-          ["<C-u>"] = "preview-page-up",
-          ["<C-d>"] = "preview-page-down",
-        }
-      },
-      fzf_colors = true,
-      winopts = {
-        border = "single",
-        fullscreen = true,
-        preview = {
-          title = false,
-          scrollbar = false,
-          winopts = {
-            number = false
-          }
-        }
-      },
-      files = {
-        cwd_prompt = false,
-        formatter = "path.filename_first",
-      },
-      grep = {
-        formatter = "path.filename_first",
+  opts = {
+    file_ignore_patterns = { ".git/", "node_modules/", "e2e/" },
+    keymap = {
+      builtin = {
+        ["<C-u>"] = "preview-page-up",
+        ["<C-d>"] = "preview-page-down",
       }
-    })
-  end
+    },
+    fzf_colors = true,
+    winopts = {
+      border = "single",
+      fullscreen = true,
+      preview = {
+        title = false,
+        scrollbar = false,
+        winopts = {
+          number = false
+        }
+      }
+    },
+    files = {
+      cwd_prompt = false,
+      formatter = "path.filename_first",
+    },
+    grep = {
+      formatter = "path.filename_first",
+    }
+  },
 }
