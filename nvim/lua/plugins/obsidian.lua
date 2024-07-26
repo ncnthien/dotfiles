4c65cb13686a0824417d3ab1549af2039000ebb1
ncnthien <nhatthien185@gmail.com>
2024-07-26 10:31:56 +0700

    main: add custom frontmatter for obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -49,6 +49,21 @@ return {
         end
         return tostring(os.date("%y%m%d%H%M")) .. "-" .. suffix
       end,
+      note_frontmatter_func = function(note)
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        local out = { id = note.id, aliases = note.aliases, hubs = {} }
+
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
     })
   end,
 }
