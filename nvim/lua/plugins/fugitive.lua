b7841e23c6e64501bdcb9b28b2965ef0fa2132a3
ncnthien <nhatthien185@gmail.com>
2024-08-23 13:13:21 +1000

    main: remove unused keymaps

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -7,9 +7,6 @@ return {
     { "<leader>ga",  "<cmd>Git add .<cr>",          desc = "Git add all" },
     { "<leader>glg", "<cmd>Git log<cr>",            desc = "Git log" },
     { "<leader>gb",  "<cmd>Git branch<cr>",         desc = "Git branch" },
-    { "<leader>gc",  "<cmd>Git commit<cr>",         desc = "Git commit" },
-    { "<leader>gp",  "<cmd>Git push<cr>",           desc = "Git push" },
     { "<leader>gd",  ":Gvdiffsplit!",               desc = "Git vertical diff" },
-    { "<leader>gl",  "<cmd>Git pull<cr>",           desc = "Git pull" },
   }
 }
