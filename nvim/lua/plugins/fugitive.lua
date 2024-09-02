c5428751a892e87a0cb072e7032bce78a423e159
ncnthien <nhatthien185@gmail.com>
2024-09-02 19:15:05 +1000

    main: update keymap for fugitive

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,10 +3,10 @@ return {
   name = "fugitive",
   event = "VimEnter",
   keys = {
-    { "<leader>gg",  "<cmd>Git<cr>",                desc = "Git status" },
-    { "<leader>ga",  "<cmd>Git add .<cr>",          desc = "Git add all" },
-    { "<leader>glg", "<cmd>Git log<cr>",            desc = "Git log" },
-    { "<leader>gb",  "<cmd>Git branch<cr>",         desc = "Git branch" },
-    { "<leader>gd",  ":Gvdiffsplit!",               desc = "Git vertical diff" },
+    { "<leader>gg", "<cmd>vertical Git<cr><cmd>vertical resize 70<cr>", desc = "Git status" },
+    { "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add all" },
+    { "<leader>glg", "<cmd>Git log<cr>", desc = "Git log" },
+    { "<leader>gb", "<cmd>Git branch<cr>", desc = "Git branch" },
+    { "<leader>gd", ":Gvdiffsplit!", desc = "Git vertical diff" },
   }
 }
