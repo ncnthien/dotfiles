26785013ac45706c149d02ba37d11ed73fd085ba
ncnthien <nhatthien185@gmail.com>
2025-02-10 04:10:52 +1100

    main: update config for Snacks.picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -24,6 +24,8 @@ local dashboard = {
   },
 }
 
+local exclue = { ".git/", "node_modules/", "e2e/" }
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
@@ -33,15 +35,15 @@ return {
     dashboard = dashboard,
   },
   keys = {
-    { "<leader>ff", function() Snacks.picker.files({ exclude = { ".git/", "node_modules/", "e2e/" } }) end, desc = "Files" },
-    { "<leader>fl", function() Snacks.picker.grep({ exclude = { ".git/", "node_modules/", "e2e/" } }) end, desc = "Grep" },
-    { "<leader>sw", function() Snacks.picker.grep_word({ exclude = { ".git/", "node_modules/", "e2e/" } }) end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>ff", function() Snacks.picker.files({ exclude = exclue }) end, desc = "Files" },
+    { "<leader>fl", function() Snacks.picker.grep({ exclude = exclue }) end, desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word({ exclude = exclue }) end, desc = "Visual selection or word", mode = { "n", "x" } },
     { "<leader>fc", function() Snacks.picker.grep_word({
       search = function()
         return "<<<<<<<"
       end
     }) end, desc = "Search conflict" },
-    { "<leader>fa", function() vim.lsp.buf.code_action() end, { noremap = true, silent = true, description = "code action" } },
+    { "<leader>fa", function() vim.lsp.buf.code_action() end, silent = true, desc = "Code action" },
     { "<leader>fe", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
   }
 }
