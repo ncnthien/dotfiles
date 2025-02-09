d217eb465ca2b81d94db8699343f96b36cf0ac59
ncnthien <nhatthien185@gmail.com>
2025-02-09 19:45:43 +1100

    main: update dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -19,8 +19,8 @@ local dashboard = {
   enabled = true,
   sections = {
     { section = "header" },
-    { section = "keys", gap = 1, padding = 1 },
-    { section = "startup" },
+    { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
+    { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 }
   },
 }
 
