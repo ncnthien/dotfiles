7de30cd30f79dcf183931f185403669943a03044
ncnthien <nhatthien185@gmail.com>
2025-02-19 03:22:17 +1100

    main: add grapple

---
+++ nvim/lua/plugins/grapple.lua
@@ -0,0 +1,17 @@
+return {
+  "cbochs/grapple.nvim",
+  dependencies = { { "nvim-tree/nvim-web-devicons", lazy = true } },
+  opts = { scope = "git", },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = "Grapple",
+  keys = {
+    { "ma", "<cmd>Grapple tag index=1<cr>", desc = "Grapple tag with index=1" },
+    { "ms", "<cmd>Grapple tag index=2<cr>", desc = "Grapple tag with index=2" },
+    { "md", "<cmd>Grapple tag index=3<cr>", desc = "Grapple tag with index=3" },
+    { "mf", "<cmd>Grapple tag index=4<cr>", desc = "Grapple tag with index=4" },
+    { "'a", "<cmd>Grapple select index=1<cr>", desc = "Grapple select index=1" },
+    { "'s", "<cmd>Grapple select index=2<cr>", desc = "Grapple select index=2" },
+    { "'d", "<cmd>Grapple select index=3<cr>", desc = "Grapple select index=3" },
+    { "'f", "<cmd>Grapple select index=4<cr>", desc = "Grapple select index=4" },
+  },
+}
