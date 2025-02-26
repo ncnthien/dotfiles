655ae82af98246abcc5e870b9341c6b5e8db1aeb
ncnthien <nhatthien185@gmail.com>
2025-02-26 15:24:31 +1100

    main: add commit message generation to CodeCompanion prompt library
    This change adds a new prompt to the CodeCompanion plugin's prompt library that helps users generate commit messages following the Conventional Commit specification. The prompt uses `git diff --staged` output and the current branch name from Fugitive to generate appropriate commit messages.

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -6,6 +6,37 @@ return {
   },
   config = true,
   opts = {
+    prompt_library = {
+      ["Generate a Commit Message"] = {
+        prompts = {
+          {
+            role = "user",
+            content = function()
+              return string.format(
+                [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+
+  The current branch is %s.
+  The format of commit title should be "<current_branch>: <description>"
+  For example: if the current branch is "feature/1234", the change is add "creating books"
+    - The valid commit title should be "feature/1234: add books feature".
+    - The invalid commit title should be "feature/1234: feat(book-feature): add new feature".
+  The above commit title is invalid because it contains more than one colon.
+  ]],
+                vim.fn.system("git diff --no-ext-diff --staged"),
+                vim.fn.FugitiveHead()
+              )
+            end,
+            opts = {
+              contains_code = true,
+            },
+          },
+        },
+      }
+    },
     adapters = {
       copilot = function()
         return require("codecompanion.adapters").extend("copilot", {
