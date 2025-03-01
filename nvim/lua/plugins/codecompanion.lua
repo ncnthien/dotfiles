1e8d4875de43a3490a018a549aef4fc16117486b
ncnthien <nhatthien185@gmail.com>
2025-03-02 04:14:29 +1100

    main: refactor codecompanion configuration
    Changed from using `config = true, opts = {...}` pattern to explicitly calling the setup function with `config = function() require("codecompanion").setup({...}) end`. Content of the configuration remains the same.

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -4,16 +4,16 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = true,
-  opts = {
-    prompt_library = {
-      ["Generate a Commit Message"] = {
-        prompts = {
-          {
-            role = "user",
-            content = function()
-              return string.format(
-                [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+  config = function ()
+    require("codecompanion").setup({
+      prompt_library = {
+        ["Generate a Commit Message"] = {
+          prompts = {
+            {
+              role = "user",
+              content = function()
+                return string.format(
+                  [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
 
 ```diff
 %s
@@ -26,51 +26,52 @@ return {
     - The invalid commit title should be "feature/1234: feat(book-feature): add new feature".
   The above commit title is invalid because it contains more than one colon.
   ]],
-                vim.fn.system("git diff --no-ext-diff --staged"),
-                vim.fn.FugitiveHead()
-              )
-            end,
-            opts = {
-              contains_code = true,
+                  vim.fn.system("git diff --no-ext-diff --staged"),
+                  vim.fn.FugitiveHead()
+                )
+              end,
+              opts = {
+                contains_code = true,
+              },
             },
           },
-        },
-      }
-    },
-    adapters = {
-      copilot = function()
-        return require("codecompanion.adapters").extend("copilot", {
-          schema = {
-            model = {
-              default = "claude-3.7-sonnet-thought"
+        }
+      },
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.7-sonnet-thought"
+              }
             }
-          }
-        })
-      end
-    },
-    strategies = {
-      chat = {
-        roles = {
-          llm = function (adapter)
-            return adapter.formatted_name
-          end
-        },
-        slash_commands = {
-          ["file"] = {
-            opts = {
-              provider = "snacks"
+          })
+        end
+      },
+      strategies = {
+        chat = {
+          roles = {
+            llm = function (adapter)
+              return adapter.formatted_name
+            end
+          },
+          slash_commands = {
+            ["file"] = {
+              opts = {
+                provider = "snacks"
+              }
             }
           }
         }
-      }
-    },
-    display = {
-      chat = {
-        -- show_header_separator = true, This option cause a bug that we cannot insert buffer into the chat
-        window = {
-          width = 70
+      },
+      display = {
+        chat = {
+          -- show_header_separator = true, This option cause a bug that we cannot insert buffer into the chat
+          window = {
+            width = 70
+          }
         }
       }
-    }
-  },
+    })
+  end
 }
