99cbaf06d95c4e6a5ff4f84e18c146750290cde9
ncnthien <nhatthien185@gmail.com>
2025-05-22 10:32:58 +0700

    main: Add keyboard shortcut for opening Diffview
    - Add new keybinding '<leader>do' to open Diffview
    - Improve git workflow capabilities by complementing existing diffview commands
    - Enhance user experience with complete set of Diffview operations

--- nvim/lua/plugins/diff-view.lua
+++ nvim/lua/plugins/diff-view.lua
@@ -3,7 +3,8 @@ return {
   name = "diffview",
   keys = {
     { "<leader>df", ":DiffviewFileHistory %", desc = "Diffview current file history" },
-    { "<leader>dd", "<cmd>DiffviewClose<cr>", desc = "Diffview close" }
+    { "<leader>dd", "<cmd>DiffviewClose<cr>", desc = "Diffview close" },
+    { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Diffview open" }
   },
   opts = { use_icons = false }
 }
